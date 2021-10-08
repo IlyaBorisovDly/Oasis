36adc28952d9fb41483619646f548cc0a9ef6b08
IlyaBorisovDly <76851543+IlyaBorisovDly@users.noreply.github.com>
2021-10-08 14:26:07 +0300

    Added utils

---
+++ app/src/main/java/com/example/oasis/utils/ActivityUtils.kt
@@ -0,0 +1,8 @@
+package com.example.oasis.utils
+
+import android.app.Activity
+import android.widget.Toast
+
+fun Activity.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(applicationContext, message, duration).show()
+}
